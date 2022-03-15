f44179c51504d40fc5034631882c2e10541a2832
shim506 <shim506@naver.com>
2022-03-15 10:52:35 +0900

    REFACTOR : 함수명 수정 및 코드 간결화

---
+++ app/src/main/java/com/example/kotlin_drawingapp/data/Text.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_drawingapp.data
+
+data class Text(val text: String, val rec: Rectangle)
+
--- app/src/main/java/com/example/kotlin_drawingapp/data/model/Plane.kt
+++ app/src/main/java/com/example/kotlin_drawingapp/data/model/Plane.kt
@@ -2,17 +2,18 @@ package com.example.kotlin_drawingapp.data.model
 
 import com.example.kotlin_drawingapp.PlaneImageAddListener
 import com.example.kotlin_drawingapp.PlaneRectangleAddListener
-import com.example.kotlin_drawingapp.data.Picture
-import com.example.kotlin_drawingapp.data.Point
-import com.example.kotlin_drawingapp.data.Rectangle
-import com.example.kotlin_drawingapp.data.RectangleFactory
+import com.example.kotlin_drawingapp.data.*
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.Logger.d
 
 object Plane {
     var rectangleList = mutableListOf<Rectangle>()
     var selectedRecList = mutableListOf<Rectangle>()
     var selectedRec: Rectangle? = null
     var selectedPicture: Picture? = null
+    var selectedText : Text? = null
     val pictureList = mutableListOf<com.example.kotlin_drawingapp.data.Picture>()
+    val textList = mutableListOf<Text>()
 
     fun getRectangleCount(): Int {
         return rectangleList.size
@@ -26,6 +27,7 @@ object Plane {
     fun addRectangle(rectangle: Rectangle, addListener: PlaneRectangleAddListener) {
         rectangleList.add(rectangle)
         addListener.onEvent(rectangleList)
+        Logger.d(rectangleList)
     }
 
     fun addImageRectangle(
@@ -41,6 +43,7 @@ object Plane {
         val allRectangleList = mutableListOf<Rectangle>()
         allRectangleList.addAll(rectangleList)
         allRectangleList.addAll(pictureList.map { it.rec })
+    //    allRectangleList.addAll(textList.map { it.rec })
 
         if (existRecOrPic(x, y, allRectangleList)) {
             selectedRecList.clear()
@@ -48,6 +51,7 @@ object Plane {
         } else {
             rectangleList.forEach {
                 if (pointInRectangle(it, x, y)) {
+                    Logger.d("sdddd")
                     selectedRecList.add(it)
                     selectedRec = it
                     selectedPicture = null
@@ -62,6 +66,14 @@ object Plane {
                     return
                 }
             }
+//            textList.forEach {
+//                if (pointInRectangle(it.rec, x, y)) {
+//                    selectedRecList.add(it.rec)
+//                    selectedRec = it.rec
+//                    selectedText = it
+//                    return
+//                }
+//            }
         }
     }
 
@@ -84,6 +96,10 @@ object Plane {
 
     fun moveRectangle(rectangle: Rectangle?, x: Int, y: Int) {
         rectangle?.point = Point(x, y)
+    }
+
+    fun addText(randomText: Text) {
+        textList.add(randomText)
     }
 
 
